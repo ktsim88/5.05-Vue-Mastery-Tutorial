17b4248ff49c48546bfb6a9878c983848c73fc9c
ktsim88 <526ktsim@frhsd.com>
2025-11-26 12:44:05 -0500

    Conditional Rendering

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://github.com/ktsim88/5.05-Vue-Mastery-Tutorial'
+            inventory: 100,
+            onSale: true
         }
     }
 })
