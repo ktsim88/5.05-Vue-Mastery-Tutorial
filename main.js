18fa04ef779c5c6df1188e86a9749b3b7c759295
ktsim88 <526ktsim@frhsd.com>
2025-11-26 12:51:32 -0500

    List Rendering

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 100,
-            onSale: true
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2335, color: 'blue'}
+            ],
+            sizes: [
+                { id: 1, size: 'XS' },
+                { id: 2, size: 'S'}
+            ]
         }
     }
 })
