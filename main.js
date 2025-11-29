a180a897715ca2f4119af02c33b3cd332c8c82f6
ktsim88 <526ktsim@frhsd.com>
2025-11-29 16:17:49 +0000

    Event handling

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2335, color: 'blue'}
-            ],
-            sizes: [
-                { id: 1, size: 'XS' },
-                { id: 2, size: 'S'}
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(varientImage) { 
+            this.image = varientImage
+        },
+        removeFromCart() { 
+            this.cart -= 1
         }
     }
 })
